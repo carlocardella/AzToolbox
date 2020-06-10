73d2f4bd3e38676418527a5c40d39917285e88a8
Carlo Cardella <carloc@microsoft.com>
2020-06-10 16:01:48 -0700

    add some functions, first few tests

---
+++ build.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding(DefaultParameterSetName = 'patch')]
+param (
+    [parameter(ParameterSetName = 'major')]
+    [switch]$Major,
+
+    [parameter(ParameterSetName = 'minor')]
+    [switch]$Minor,
+
+    [parameter(ParameterSetName = 'patch')]
+    [switch]$Patch
+)
+
+if (!(Get-Module -ListAvailable 'Pster').Version.Major -contains 5) {
+    Install-Module -Name 'Pester' -Scope 'CurrentUser' -Force -AcceptLicense -AllowClobber -SkipPublisherCheck -Repository 'PSGallery'
+}
+
+Import-Module -Name 'Pester' -MinimumVersion [String.Version]::new(5, 0)
+
+Invoke-Pester -Path "$PSScriptRoot/src/AzureOps"
