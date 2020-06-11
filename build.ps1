622be2c3144f24597f999a436f6359f8983a8d4c
Carlo Cardella <carloc@microsoft.com>
2020-06-11 13:23:13 -0700

    make sure Pester v5+ is installed

--- build.ps1
+++ build.ps1
@@ -10,9 +10,9 @@ param (
     [switch]$Patch
 )
 
-if (!(Get-Module -ListAvailable 'Pester').Version.Major -contains 5) {
-    Install-Module -Name 'Pester' -Force -AcceptLicense -AllowClobber -SkipPublisherCheck -Repository 'PSGallery' -Verbose -MinimumVersion '5.0.0.0' -ErrorAction 'Stop'
-}
+# if (!(Get-Module -ListAvailable 'Pester').Version.Major -contains 5) {
+Install-Module -Name 'Pester' -Force -AcceptLicense -AllowClobber -SkipPublisherCheck -Repository 'PSGallery' -Verbose -MinimumVersion '5.0.0.0' -ErrorAction 'Stop'
+# }
 
 Import-Module -Name 'Pester' -MinimumVersion $([System.Version]::new(5, 0))
 
