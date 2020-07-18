fc8fb9364c633266ec2cb4c56ef5aa625229e9ee
Carlo Cardella <carloc@microsoft.com>
2020-07-17 22:14:59 -0700

    add Save-AzCliReleaseAsset

---
+++ src/AzToolbox/Save-AzCliReleaseAsset.ps1
@@ -0,0 +1,57 @@
+function Save-AzCliReleaseAsset {
+    [CmdletBinding()]
+    param (
+        [parameter()]
+        [ValidateSet('MSI', 'HomebrewFormula', 'UbuntuXenialDeb', 'UbuntuBionicDeb', 'RPM')]
+        [string]$Package,
+
+        [parameter()]
+        [ValidateScript( { Test-Path -Path $_ -PathType 'Container' })]
+        [string]$OutputFolder
+    )
+
+    $asset = Get-AzCliReleaseAsset
+
+    switch ($Package) {
+        MSI {
+            $uri = 'https://aka.ms/InstallAzureCliWindowsEdge'
+            Write-Verbose $uri
+            # $destination = Join-Path -Path $OutputFolder -ChildPath 'Microsoft Azure CLI.msi'
+            $destination = Join-Path -Path $OutputFolder -ChildPath ("{0}{1}" -f $asset.Tag, '.msi')
+        }
+        
+        HomebrewFormula {
+            $uri = 'https://aka.ms/InstallAzureCliHomebrewEdge' 
+            Write-Verbose $uri
+            # $destination = Join-Path -Path $OutputFolder -ChildPath 'azure-cli.rb'
+            $destination = Join-Path -Path $OutputFolder -ChildPath ("{0}{1}" -f $asset.Tag, '.rb')
+        }
+        
+        UbuntuXenialDeb {
+            $uri = 'https://aka.ms/InstallAzureCliXenialEdge' 
+            Write-Verbose $uri
+            # $destination = Join-Path -Path $OutputFolder -ChildPath 'azure-cli_xenial_all.deb'
+            $destination = Join-Path -Path $OutputFolder -ChildPath ("{0}{1}" -f $asset.Tag, '.deb')
+        }
+        
+        UbuntuBionicDeb {
+            $uri = 'https://aka.ms/InstallAzureCliBionicEdge' 
+            Write-Verbose $uri
+            # $destination = Join-Path -Path $OutputFolder -ChildPath 'azure-cli_bionic_all.deb'
+            $destination = Join-Path -Path $OutputFolder -ChildPath ("{0}{1}" -f $asset.Tag, '.deb')
+        }
+        
+        RPM {
+            $uri = 'https://aka.ms/InstallAzureCliRpmEdge' 
+            Write-Verbose $uri
+            # $destination = Join-Path -Path $OutputFolder -ChildPath 'azure-cli.rpm'
+            $destination = Join-Path -Path $OutputFolder -ChildPath ("{0}{1}" -f $asset.Tag, '.rpm')
+        }
+        
+        Default {
+            Write-Error "Invalid Package type: $Package"
+        }
+    }
+
+    Invoke-Webrequest -Uri $uri -UseBasicParsing -DisableKeepAlive -OutFile $destination
+}
