6e85e6a1dc1950fac3eba9b14f06b3118ccafcf9
Carlo Cardella <carloc@microsoft.com>
2020-07-21 08:24:22 -0700

    add Get-AzClientCertificate, Get-AzResourceProviderApiVersion; add custom types and formatters; add tests; increase Minor version

--- Tests/AzureToolbox.Tests.ps1
+++ Tests/AzureToolbox.Tests.ps1
@@ -11,7 +11,7 @@ Describe 'AzToolbox' {
             { Get-AzCliReleaseAsset } | Should -Not -Throw
             @(Get-AzCliReleaseAsset).Count | Should -BeGreaterOrEqual 1
         }
-        
+
         It 'Returns a proper AzCliReleaseType object' {
             (Get-AzCliReleaseAsset | Get-Member).TypeName[0] | Should -BeExactly 'AzCliReleaseAsset'
             $assetObject = Get-AzCliReleaseAsset
@@ -27,5 +27,44 @@ Describe 'AzToolbox' {
         It 'Can download Az CLI release asset' {
             { Save-AzCliReleaseAsset -Package 'MSI' -OutputFolder $TestDrive } | Should -Not -Throw
         }
+    }
+
+    Context -Name 'Get-AzClientCertificate' -Tag 'GetAzClientCertificate' {
+        It 'Can retrieve the Public ARM certificate using the default parameter' {
+            { Get-AzClientCertificate } | Should -Not -Throw
+            (Get-AzClientCertificate).Certificate  | Should -Not -BeNullOrEmpty
+            @(Get-AzClientCertificate).Count | Should -Not -BeGreaterThan 2
+        }
+
+        It 'Returns an error for an invalid Azure Cloud name' {
+            Get-AzClientCertificate -Environment 'NonExistingCloud' -ErrorVariable 'err' -ErrorAction 'SilentlyContinue'
+            $err.Count | Should -Not -Be 0
+            $err[0].Exception.Message | Should -Be 'Invalid envorinment: NonExistingCloud'
+
+        }
+
+        It 'Can retrieve the ARM certificate for <Name>' {
+            { Get-AzClientCertificate $Name } | Should -Not -Throw
+            (Get-AzClientCertificate $Name).Certificate  | Should -Not -BeNullOrEmpty
+            @(Get-AzClientCertificate $Name).Count | Should -Not -BeGreaterThan 2
+
+        } -TestCases @(
+            Get-AzEnvironment | ForEach-Object {
+                @{'Name' = $_.Name }
+            }
+        ) -Tag 'Slow'
+    }
+
+    Context -Name 'Get-AzResourceProviderApiVersion' -Tag 'GetAzResourceProviderApiVersion', 'Slow' {
+        # todo: mock Get-AzResourceProvider
+        It "Returns API version for <RP>" {
+            { Get-AzResourceProviderApiVersion $RP } | Should -Not -Throw
+            $ov = Get-AzResourceProviderApiVersion -ProviderNamespace $RP
+            $ov.Count | Should -BeGreaterThan 1
+        } -TestCases @(
+            @{'RP' = 'Microsoft.Automation' },
+            @{'RP' = 'Microsoft.ContainerService' },
+            @{'RP' = 'Microsoft.Storage' }
+        )
     }
 }
---
+++ src/AzToolbox/Get-AzClientCertificate.ps1
@@ -0,0 +1,86 @@
+function Get-AzClientCertificate {
+    <#
+    .SYNOPSIS
+    Returns the Azure Resource Manager client authentication certificate for the selected environment
+    The response can list one or two certificates: if two are presented, both certificates
+    should be trusted to permit the certificate rolling use case.
+
+    The "certificate" field is a Base64 encoded cer file, if RP want to read and validate more fields.
+
+    .PARAMETER Environment
+    The Azure Cloud environment name to query
+
+    .EXAMPLE
+    Get-AzClientCertificate -Environment Public
+
+    thumbprint                               notBefore            notAfter             certificate
+    ----------                               ---------            --------             -----------
+    6510AFE49C4FE1ADB0CCC0B65BAB07C298E6609A 2015-08-18T22:16:02Z 2017-08-17T22:16:02Z MIIGaTCCBFGgAwIBAgITWgABpmv9...
+
+    Returns the available ARM client certificate(s) from the Public generic ARM endpoint
+
+    .OUTPUTS
+    Microsoft.Azure.AzureArmClientCertificate
+    #>
+    [CmdletBinding()]
+    [OutputType('AzureArmClientCertificate')]
+    param (
+        [parameter(Position = 0)]
+        [ArgumentCompleter( {
+                param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+                Get-AzEnvironment | Select-Object -ExpandProperty 'Name'
+            })]
+        [string]$Environment = 'AzureCloud'
+    )
+
+    process {
+        if (![string]::IsNullOrWhiteSpace($PSCmdlet.MyInvocation.BoundParameters.RegionalEndpoint)) {
+            $re = "{0}." -f $PSCmdlet.MyInvocation.BoundParameters.RegionalEndpoint
+        }
+
+        $uri = $null
+        $outObj = $null
+        if ($IsCoreCLR) {
+            [Microsoft.Powershell.Commands.BasicHtmlWebResponseObject]$response = $null
+        }
+        else {
+            [Microsoft.PowerShell.Commands.HtmlWebResponseObject]$response = $null
+        }
+        switch ($Environment) {
+            'AzureCloud' {
+                $uri = "https://management.azure.com:24582/metadata/authentication?api-version=2015-01-01"
+                $response = Invoke-WebRequest -Uri $uri -UseDefaultCredentials
+                $outObj = $response | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty 'clientCertificates'
+            }
+
+            'AzureChinaCloud' {
+                $uri = "https://management.chinacloudapi.cn:24582/metadata/authentication?api-version=2015-01-01"
+                $response = Invoke-WebRequest -Uri $uri -UseDefaultCredentials
+                $outObj = $response | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty 'clientCertificates'
+            }
+
+            'AzureUSGovernment' {
+                $uri = "https://management.usgovcloudapi.net:24582/metadata/authentication?api-version=2015-01-01"
+                $response = Invoke-WebRequest -Uri $uri -UseDefaultCredentials -Method 'Get'
+                $outObj = $response | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty 'clientCertificates'
+            }
+
+            'AzureGermanCloud' {
+                $uri = "https://management.microsoftazure.de:24582/metadata/authentication?api-version=2015-01-01"
+                $response = Invoke-WebRequest -Uri $uri -UseDefaultCredentials
+                $outObj = $response | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty 'clientCertificates'
+            }
+
+            Default {
+                Write-Error -Message "Invalid envorinment: $Environment"
+            }
+        }
+
+        if ($outObj) {
+            foreach ($certificate in $outObj) {
+                $certificate.PSObject.TypeNames.Insert(0, "AzureArmClientCertificate")
+                $certificate
+            }
+        }
+    }
+}
