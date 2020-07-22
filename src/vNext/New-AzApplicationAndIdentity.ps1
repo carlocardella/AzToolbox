656de19fb23e8ce95b26e4696036cea5027d7886
Carlo Cardella <carloc@microsoft.com>
2020-07-22 14:06:14 -0700

    start adding cert file and object to New-AzApplicationAndIdentity

--- src/vNext/New-AzApplicationAndIdentity.ps1
+++ src/vNext/New-AzApplicationAndIdentity.ps1
@@ -23,8 +23,16 @@ function New-AzApplicationAndIdentity {
         [parameter(Mandatory, Position = 0)]
         [string]$DisplayName,
 
-        [parameter(Mandatory, Position = 1)]
+        [parameter(Mandatory, ParameterSetName = 'password')]
         [securestring]$Password,
+
+        [parameter(Mandatory, ParameterSetName = 'certificate')]
+        [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
+
+        [parameter(Mandatory, ParameterSetName = 'certificatefile')]
+        [ValidateScript( { Test-Path -Path $_ -PathType 'Leaf' })]
+        [ValidateScript( { (Split-Path -Path $_ -Extension) -eq '.cer' })]
+        [string]$CertificatePath,
 
         [parameter()]
         [string]$Homepage,
@@ -36,9 +44,18 @@ function New-AzApplicationAndIdentity {
 
     $azureAppParams = @{
         'DisplayName'    = $DisplayName;
-        'Password'       = $Password;
+        # 'Password'       = $Password;
         'IdentifierUris' = $Homepage
     }
+    if ($Password) { $azureAppParams.Password = $Password }
+    if ($CertificatePath) {
+        $CertificatePath = Resolve-Path $CertificatePath
+        $Cer = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2($CertificatePath)
+        $BinCert = $Cer.GetRawCertData()
+        $CredValue = [System.Convert]::ToBase64String($BinCert)
+        $azureAppParams.CertValue = $CredValue
+    }
+    if ($Certificate) { throw [System.NotImplementedException]::new() }
     if ($Homepage) { $azureAppParams.Homepage = $Homepage }
     if ($MultiTenant) { $azureAppParams.AvailableToOtherTenants = $true }
 
